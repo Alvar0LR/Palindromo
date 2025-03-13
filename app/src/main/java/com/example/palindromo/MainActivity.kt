eda7e503cf243d8ca84dfd770ff8e0d64843e628
Alvaro <alujan8@alu.ucam.edu>
2025-03-13 19:16:38 +0100

    Palindromo

---
+++ app/src/main/java/com/example/palindromo/MainActivity.kt
@@ -0,0 +1,74 @@
+package com.example.palindromo
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.palindromo.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener  {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        init()
+    }
+
+    fun init(){
+        binding.Button.setOnClickListener(this)
+
+    }
+    override fun onClick(view: View?) {
+        when(view){
+
+            binding.Button -> palindromoYReversa()
+        }
+    }
+
+    fun palindromoYReversa(){
+
+        val imput = binding.EditText.text.toString()
+        var palabrareves = rever(imput)
+
+        binding.screen.text = "$palabrareves"
+        if (imput == palabrareves) {
+            binding.screen2.text = "si"
+        }else{
+            binding.screen2.text = "no"
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+
+        outState.putString("palindromo",  binding.screen2.text.toString())
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        binding.screen2.text = savedInstanceState.getString("palindromo", "")
+    }
+
+    fun rever(palabra: String): String {
+
+        var pal = ""
+
+        for(i in palabra.length-1 downTo 0 ){
+            pal += palabra[i]
+        }
+        return pal
+    }
+
+
+
+}
